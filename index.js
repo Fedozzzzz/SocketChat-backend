506c05dcb1a234fe25441258c4cb4bfeb3a688e2
FedozZz <titov.bigf@gmail.com>
2019-10-27 14:34:13 +0300

    code refactoring.

--- index.js
+++ index.js
@@ -1,28 +1,17 @@
 const app = require("express")();
-const http = require("http").createServer(app);
+const PORT = 8080;
+const http = require("http").createServer(app).listen(PORT, () => {
+    console.log(`server listening on port ${PORT} ...`)
+});
 const io = require("socket.io")(http);
 const uuid = require("uuid");
 
-const PORT = 8080;
 
 let users = [];
 let rooms = new Map();
 
 io.on("connection", (socket) => {
-    // console.log("a new user connected");
-    // io.send("a new user connected!");
-    // if (socket.rooms) {
-    // let room = uuid.v4();
-    // socket.join(room);
-    // socket.emit("all rooms", io.sockets.adapter.rooms);
-    // }
-    // let room;
     socket.on("get all rooms", () => {
-        // console.log(socket);
-        // console.log(io.sockets.adapter.rooms);
-        // socket.emit("all rooms", io.sockets.adapter.rooms)
-        // console.log(rooms);
-        // console.log(JSON.stringify(rooms));
         const obj = {};
         rooms.forEach((v, k) => {
             obj[k] = v
@@ -34,40 +23,24 @@ io.on("connection", (socket) => {
         rooms.set(roomId, roomName);
         socket.join(roomId);
         socket.currentRoom = roomId;
+        // socket.emit("get all rooms")
     });
-    // console.log(socket.rooms);
-    // socket.emit("all rooms", socket);
-    // socket.on("room", msg => {
-    //     const json = JSON.parse(msg);
-    //     users[json.id] = socket;
-    //     if (socket.room) {
-    //         socket.leave(socket.room)
-    //     }
-    //     socket.room = json.room;
-    //     socket.join(socket.room);
-    //     socket.user_id = json.id;
-    //     socket.broadcast.to(socket.room).emit("new", json.id)
-    // });
     socket.on("join room", (roomId) => {
         socket.join(roomId);
         socket.currentRoom = roomId;
-        socket.broadcast.to(roomId).emit("news", {msg: `a ${socket.nickname} user connected!`});
+        socket.broadcast.to(roomId).emit("news", {msg: `${socket.nickname} join the chat!`});
     });
     socket.on("disconnect", (userId) => {
         socket.leave(socket.currentRoom);
-        socket.broadcast.to(socket.currentRoom).emit("news", {msg: `a user ${socket.nickname} disconnected`});
+        socket.broadcast.to(socket.currentRoom).emit("news", {msg: `${socket.nickname} leave the chat`});
         socket.currentRoom = null;
     });
     socket.on("nickname", (nickname) => {
         users.push({nickname: nickname, id: socket.id});
         socket.nickname = nickname;
-        console.log(socket.nickname);
+        // console.log(socket.nickname);
     });
     socket.on("chat message", (data) => {
         io.to(socket.currentRoom).emit('chat message', `${socket.nickname}: ${data.msg}`);
     });
-});
-
-http.listen(PORT, () => {
-    console.log(`server listening on port ${PORT} ...`)
 });
